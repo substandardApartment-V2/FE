054e61c4b6648432cc772614864bafc470de035e
exceed96 <exceed96@naver.com>
2025-01-09 18:02:49 +0900

    [remove] main.module.scss파일 삭제#11

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,6 @@
 import { PropsWithChildren } from "react";
 import { Outlet } from "react-router-dom";
-import styles from "./Main.module.scss";
 
 export default function Main({ children }: PropsWithChildren) {
-  return <main className={styles.main}>{children || <Outlet />}</main>;
+  return <main>{children || <Outlet />}</main>;
 }
