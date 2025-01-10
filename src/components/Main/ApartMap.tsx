2f3d9e408d3bec05b06cb7359bb9cb1a6a2fa333
exceed96 <exceed96@naver.com>
2025-01-10 19:01:00 +0900

    [Feat] 네이버 지도 연결 #11

--- src/components/Main/ApartMap.tsx
+++ src/components/Main/ApartMap.tsx
@@ -1,5 +1,47 @@
+import { useRef, useState, useEffect } from "react";
+import MapLoading from "./MapLoading";
 import styles from "./ApartMap.module.scss";
 
 export default function ApartMap() {
-  return <section className={styles.apartMap}></section>;
+  const mapRef = useRef<HTMLDivElement | null>(null);
+  const [mapLoading, setMapLoading] = useState<boolean>(true);
+
+  const getSuccess = (position: GeolocationPosition) => {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const { naver } = window;
+    if (mapRef.current && naver) {
+      const location = new naver.maps.LatLng(lat, lng);
+      const map = new naver.maps.Map(mapRef.current, {
+        center: location,
+        zoom: 15,
+      });
+    }
+  };
+
+  const getError = () => {
+    console.log("location error");
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(getSuccess, getError);
+  }, []);
+
+  return (
+    <section className={styles.apartMap}>
+      {/* {mapLoading ? ( */}
+      <>
+        <div>
+          {/* <img src={MapSearch} alt="search region icon" className="mr-3.5" /> */}
+          {/* <span className="text-white font-[Pretendard-SemiBold] xxs:text-xs xs:text-base sm:text-lg md:text-xl">
+              {mapLocation.roadAddress}
+            </span> */}
+        </div>
+        <div id="map" ref={mapRef} className={styles.map} />
+      </>
+      {/* ) : ( */}
+      {/* <MapLoading /> */}
+      {/* )} */}
+    </section>
+  );
 }
--- src/components/Main/Home/Home.tsx
+++ src/components/Main/Home/Home.tsx
@@ -1,8 +1,20 @@
 import styles from "./Home.module.scss";
 import ApartMap from "../ApartMap";
 import searchIcon from "@/assets/Main/searchICon.svg";
+import DropDown from "@/components/Abstraction/DropDown/DropDown";
 
 export default function Home() {
+  const dropDownContents = [
+    {
+      content: "최신순",
+      contentFn: () => {},
+    },
+    {
+      content: "과거순",
+      contentFn: () => {},
+    },
+  ];
+
   return (
     <section className={styles.home}>
       <section className={styles.homeRight}>
@@ -53,7 +65,47 @@ export default function Home() {
           </div>
         </div>
         <div className={styles.serviceNotice}>
-          <h1>공지사항</h1>
+          <div className={styles.serviceNoticeTitle}>
+            <strong>공지사항</strong>
+            <DropDown
+              select="최신순"
+              fontSize="MEDIUM"
+              outerBorder={true}
+              dropDownContents={dropDownContents}
+            />
+          </div>
+          <ul className={styles.noticeLists}>
+            <li>
+              <div className={styles.noticeListTitle}>
+                <strong>사이트 긴급점검 시간 공지 2025.01.20~01.21</strong>
+                <span>2025.01.20</span>
+              </div>
+              <div className={styles.noticeListContent}>
+                사이트 긴급점검 시간 공지드립니다. 1월 20일 13시~21일 7시까지
+                점검이 있을 예정이오니 이점 유의 바랍니다.
+              </div>
+            </li>
+            <li>
+              <div className={styles.noticeListTitle}>
+                <strong>사이트 긴급점검 시간 공지 2025.01.20~01.21</strong>
+                <span>2025.01.20</span>
+              </div>
+              <div className={styles.noticeListContent}>
+                사이트 긴급점검 시간 공지드립니다. 1월 20일 13시~21일 7시까지
+                점검이 있을 예정이오니 이점 유의 바랍니다.
+              </div>
+            </li>
+            <li>
+              <div className={styles.noticeListTitle}>
+                <strong>사이트 긴급점검 시간 공지 2025.01.20~01.21</strong>
+                <span>2025.01.20</span>
+              </div>
+              <div className={styles.noticeListContent}>
+                사이트 긴급점검 시간 공지드립니다. 1월 20일 13시~21일 7시까지
+                점검이 있을 예정이오니 이점 유의 바랍니다.
+              </div>
+            </li>
+          </ul>
         </div>
       </section>
       <ApartMap />
