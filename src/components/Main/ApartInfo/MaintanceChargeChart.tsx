5532539957a9b2dce03748958b403cd8a09a3e24
exceed96 <exceed96@naver.com>
2025-01-11 22:12:45 +0900

    [Feat] 아파트 마커 클릭 정보 퍼블리싱 #11

---
+++ src/components/Main/ApartInfo/MaintanceChargeChart.tsx
@@ -0,0 +1,86 @@
+import { Bar } from "react-chartjs-2";
+import "chart.js/auto";
+import { plugins } from "chart.js/auto";
+
+const MaintanceChargeChart = () => {
+  const area = "59㎡";
+  const data = {
+    labels: [
+      "1월",
+      "2월",
+      "3월",
+      "4월",
+      "5월",
+      "6월",
+      "7월",
+      "8월",
+      "9월",
+      "10월",
+      "11월",
+      "12월",
+    ],
+    datasets: [
+      {
+        label: `관리비 개별사용료 - ${area}`,
+        data: [
+          3006, 2010, 1015, 2540, 2000, 4000, 12340, 3000, 2200, 2300, 2400,
+          4322,
+        ],
+        backgroundColor: ["#E4E1D0"],
+        barThickness: 17,
+        barPercentage: 0.5,
+        maxBarThickness: 8,
+        minBarLength: 3,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        grid: {
+          color: function (context: any) {
+            if (context.tick.value === 0) {
+              return "#ffffff";
+            }
+            return "#ffffff";
+          },
+          lineWidth: 0.5,
+        },
+        border: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value: number) {
+            return value.toLocaleString() + "원";
+          },
+          color: "#ffffff",
+          font: {
+            size: 12,
+          },
+          padding: 0,
+        },
+      },
+      x: {
+        beginAtZero: true,
+        ticks: {
+          color: "#ffffff",
+          font: {
+            size: 12,
+          },
+        },
+      },
+    },
+    plugins: {
+      title: {
+        display: false,
+        text: "Custom Chart Subtitle",
+      },
+    },
+  };
+
+  return <Bar data={data} options={options} />;
+};
+
+export default MaintanceChargeChart;
--- src/pages/ApartInfoPage.tsx
+++ src/pages/ApartInfoPage.tsx
@@ -1,12 +1,16 @@
 // import Home from "@/components/Main/Home/Home";
+import Home from "@/components/Main/Home/Home";
+import styles from "./ApartInfoPage.module.scss";
 import ApartInfo from "@/components/Main/ApartInfo/ApartInfo";
+import ApartMap from "@/components/Main/ApartMap";
 
 const ApartInfoPage = () => {
   return (
-    <>
+    <div className={styles.home}>
       {/* <Home /> */}
       <ApartInfo />
-    </>
+      <ApartMap />
+    </div>
   );
 };
 
