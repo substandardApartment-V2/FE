22876bed4f58f92fe8fc034799510db11ac805f2
exceed96 <exceed96@naver.com>
2025-01-21 11:44:52 +0900

    [style] 면적 별 관리비 드랍다운 클래스명 변경 및 스타일 변경#11

--- src/components/Main/ApartInfo/DetailMaintanceCharge.tsx
+++ src/components/Main/ApartInfo/DetailMaintanceCharge.tsx
@@ -18,8 +18,8 @@ export default function DetailMaintanceCharge() {
     },
   ];
   return (
-    <section className={styles.maintance}>
-      <section>
+    <section className={styles.maintances}>
+      <section className={styles.eachArea}>
         <div>
           <div>
             <span className={styles.title}>
@@ -43,13 +43,13 @@ export default function DetailMaintanceCharge() {
         </div>
         <MaintanceChargeChart />
       </section>
-      <section>
+      <section className={styles.eachArea}>
         <span className={styles.title}>
           관리비 개별사용료 - <span>82㎡</span>
         </span>
         <MaintanceChargeChart />
       </section>
-      <section>
+      <section className={styles.eachArea}>
         <span className={styles.title}>
           관리비 개별사용료 - <span>114㎡</span>
         </span>
