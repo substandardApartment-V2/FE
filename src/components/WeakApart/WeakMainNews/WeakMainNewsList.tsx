fb593bdae0514947d357aec1dcf3b06b1bc0af12
exceed96 <exceed96@naver.com>
2025-02-21 11:41:53 +0900

    [feat] 뉴스 기본 이미지 #36

--- src/components/WeakApart/WeakMainNews/WeakMainNewsList.tsx
+++ src/components/WeakApart/WeakMainNews/WeakMainNewsList.tsx
@@ -2,6 +2,7 @@ import styles from "./WeakMainNewsList.module.scss";
 import { TWeakMainNewsList } from "@/types/TWeak/TWeakMainNewsListTypes";
 import { formatDate } from "@/utils/formatDate";
 import { Link } from "react-router-dom";
+import defaultNewsImage from "@/assets/news/newsImg01.jpeg";
 
 export default function WeakMainNewsList(props: TWeakMainNewsList) {
   const parseContent = (html: string) => {
@@ -12,7 +13,11 @@ export default function WeakMainNewsList(props: TWeakMainNewsList) {
   return (
     <li className={styles.newsContent}>
       <Link to={props.url} target="_blank">
-        <img src={props.image} className={styles.newsImage} alt="news image" />
+        <img
+          src={props.image ? props.image : defaultNewsImage}
+          className={styles.newsImage}
+          alt="news image"
+        />
         <div className={styles.newsText}>
           <span className={styles.title}>{parseContent(props.title)}</span>
           <p className={styles.content}>{parseContent(props.content)}</p>
