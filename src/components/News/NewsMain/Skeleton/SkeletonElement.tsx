7c4811ac5f53a7d0de586237b2b53a6279fadaeb
kim-yejin <yeje104@naver.com>
2025-02-24 18:29:32 +0900

    [remove] 파일 삭제

--- src/components/News/NewsMain/Skeleton/SkeletonElement.tsx
+++
@@ -1,7 +0,0 @@
-import { TSkeleton } from "@/types/TNews/TNewsSkeletonTypes";
-import styles from "./NewsSkeleton.module.scss";
-
-const SkeletonElement = ({ type }: { type: TSkeleton }) => {
-  return <div className={`${styles.skeleton} ${styles[type]}`}></div>;
-};
-export default SkeletonElement;
