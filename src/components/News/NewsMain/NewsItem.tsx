031fcf41e56a2fb74d38d391141d5cefaced963c
kim-yejin <yeje104@naver.com>
2025-02-28 16:52:55 +0900

    [feat] 뉴스 디폴트 이미지 추가

--- src/components/News/NewsMain/NewsItem.tsx
+++ src/components/News/NewsMain/NewsItem.tsx
@@ -1,3 +1,4 @@
+import defaultImg from "@/assets/News/newsDefaultImage.jpeg";
 import { TNewsItem } from "@/types/TNews/TNewsItemTypes";
 import { formatDate } from "@/utils/parse/formatDate";
 import parseContent from "@/utils/parse/parseContent";
@@ -12,10 +13,20 @@ const NewsItem = ({
   content,
   url,
 }: TNewsItem) => {
+  const onErrorImg: React.ReactEventHandler<HTMLImageElement> = (e) => {
+    const target = e.target as HTMLImageElement;
+    target.src = defaultImg;
+  };
+
   return (
     <li className={styles.newsItem}>
       <Link to={url} target="_blank" className={styles.newsItemWrap}>
-        <img src={image} alt="newsImage" className={styles.newsImgBox} />
+        <img
+          src={image || defaultImg}
+          alt="newsImage"
+          onError={onErrorImg}
+          className={styles.newsImgBox}
+        />
         <div className={styles.newsItemContentWrapper}>
           <h3 className={styles.newsItemTitle}>{parseContent(title)}</h3>
           <p className={styles.newsItemDescription}>{parseContent(content)}</p>
