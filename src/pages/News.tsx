f9292b1618d710694b4647630238c259ff724ba3
exceed96 <exceed96@naver.com>
2025-03-10 13:25:09 +0900

    [feat] title 태그 수정 #58

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -3,11 +3,16 @@ import NewsPagination from "../components/News/NewsFooter/NewsPagination";
 import NewsHeader from "../components/News/NewsHeader/NewsHeader";
 import NewsMain from "../components/News/NewsMain/NewsMain";
 import styles from "./News.module.scss";
+import { useNewsStore } from "@/store/useNewsStore";
 
 const News = () => {
+  const activeTab = useNewsStore((state) => state.activeTab);
+
   return (
     <div className={styles.news}>
-      <SEOMetaTag title="뉴스" />
+      <SEOMetaTag
+        title={activeTab === "apartNews" ? "아파트뉴스" : "부실아파트뉴스"}
+      />
       <NewsHeader />
       <NewsMain />
       <NewsPagination />
