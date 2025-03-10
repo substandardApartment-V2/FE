aeaa9700007c4a0939fd0e2d920def8107d6a87f
exceed96 <exceed96@naver.com>
2025-03-10 16:12:40 +0900

    [feat] 지도 커스텀 버튼 이미지 alt 정의#58

--- src/components/Main/WholeApartInfo/ServiceNotice/ServiceNoticeSkeleton.tsx
+++ src/components/Main/WholeApartInfo/ServiceNotice/ServiceNoticeSkeleton.tsx
@@ -4,8 +4,8 @@ import styles from "./ServiceNoticeSkeleton.module.scss";
 export default function ServiceNoticeSkeleton() {
   return (
     <div className={styles.skeletonWrapper}>
-      <SkeletonElement className={styles.title} type="title" />
-      <SkeletonElement className={styles.description} type="description" />
+      <SkeletonElement className={styles.title} />
+      <SkeletonElement className={styles.description} />
     </div>
   );
 }
