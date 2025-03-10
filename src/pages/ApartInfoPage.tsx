f546a0500ad289597fe90cc877b71f185afac2cc
exceed96 <exceed96@naver.com>
2025-03-10 12:51:21 +0900

    [feat] 파비콘 정의 #58

--- src/pages/ApartInfoPage.tsx
+++ src/pages/ApartInfoPage.tsx
@@ -3,10 +3,14 @@
 import styles from "./ApartInfoPage.module.scss";
 import InfoContainer from "@/components/Main/InfoContainer";
 import ApartMapContainer from "@/components/Main/ApartMapContainer";
+import { Helmet } from "react-helmet-async";
 
 const ApartInfoPage = () => {
   return (
     <div className={styles.apartInfoPage}>
+      <Helmet>
+        <title>내 아파트는?</title>
+      </Helmet>
       <InfoContainer />
       <ApartMapContainer />
     </div>
