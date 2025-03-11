9649a62ed85121bbb910a65f6450d7a2a0f31b36
exceed96 <exceed96@naver.com>
2025-03-11 18:04:42 +0900

    [style] 차트 툴팁 폰트 사이즈 수정#58

--- src/components/WeakApart/Chart/FiveYearsWeakBuilderChart.tsx
+++ src/components/WeakApart/Chart/FiveYearsWeakBuilderChart.tsx
@@ -9,7 +9,7 @@ export default function FiveYearsWeakBuilderChart() {
 
   useEffect(() => {
     const handleResize = () => {
-      const newFontSize = window.innerWidth < 600 ? 10 : 16;
+      const newFontSize = window.innerWidth < 600 ? 10 : 12;
       const newBarThickness = window.innerWidth < 600 ? 11 : 13;
 
       setFontSize(newFontSize);
