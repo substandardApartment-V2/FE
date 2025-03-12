148066d8793ccfa2de32586bed8db4c4521bb740
exceed96 <exceed96@naver.com>
2025-03-12 17:00:19 +0900

    [style] 관리비 관련 툴팁 디자인 1차 수정 #59

--- src/components/Main/ApartInfo/DetailMaintanceCharge.tsx
+++ src/components/Main/ApartInfo/DetailMaintanceCharge.tsx
@@ -8,6 +8,7 @@ import { TMaintanceChargeResponse } from "@/types/TApi/TAPITypes";
 import explanationIcon from "@/assets/Main/DetailInfo/explanationIcon.svg";
 import { Tooltip } from "react-tooltip";
 import ApartHeadInfo from "./ApartHeadInfo";
+import "@/App.scss";
 
 export default function DetailMaintanceCharge() {
   const setIsDetailInfo = useApartInfoStore((state) => state.setIsDetailInfo);
@@ -40,9 +41,42 @@ export default function DetailMaintanceCharge() {
                   src={explanationIcon}
                   alt="개별 사용료 설명"
                   data-tooltip-id="individual"
-                  data-tooltip-content={
-                    "난방비(공용) + 난방비(전용) + 급탕비(공용) + 급탕비(전용) + 가스사용료(공용) + 가스사용료(전용) + 전기료(공용) + 전기료(전용) + 수도료(공용) + 수도료(전용) + TV 수신료 + 정화조 오물 수수료 + 생활폐기물 수수료 + 입대의 운영비 + 건물 보험료 + 선관위 운영비 + 기타"
-                  }
+                  data-tooltip-html={`<div style="display:flex; flex-direction:column; align-items: flex-start; gap : 2rem;">
+                    <span style="color: #142337; font-size: 1.6rem;">개별 사용료 상세</span>
+                    <ul style="display: flex; flex-direction: column; gap: 1.6rem; width: 100%;">
+                     <li style="width : 100%; display: flex; gap: 1rem;">
+                      <span style="color: #ffffff; padding: 0.6rem 1.2rem; background-color: #435464; border-radius: 1.4rem; height: fit-content; width: 6rem; text-align: center;">공용</span>
+                        <ul style="display: flex; gap: 0.6rem; flex-wrap: wrap;">
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">전기료</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">수도료</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">가스사용료</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">난방비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">급탕비</li>
+                        </ul>
+                      </li>
+                      <li style="width : 100%; display: flex; gap: 1rem;">
+                      <span style="color: #ffffff; padding: 0.6rem 1.2rem; background-color: #435464; border-radius: 1.4rem; height: fit-content; width: 6rem; text-align: center;">전용</span>
+                        <ul style="display: flex; gap: 0.6rem; flex-wrap: wrap;">
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">전기료</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">수도료</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">가스사용료</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">난방비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">급탕비</li>
+                        </ul>
+                      </li>
+                      <li style="width: 100%; display: flex; gap: 1rem;">
+                        <span style="position: relative; color: #ffffff; padding: 0.6rem 1.2rem; background-color: #435464; border-radius: 1.4rem; height: fit-content; width: 9rem; text-align: center;">일반</span>
+                        <ul style="display: flex; gap: 0.6rem; flex-wrap: wrap;">
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">TV수신료</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">정화조오물 수수료</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">생활폐기물 수수료</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">건물보혐료</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">선관위운영비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">입대의 운영비</li>
+                        </ul>
+                      </li>
+                    </ul>
+                    <div>`}
                 />
                 <Tooltip
                   className={styles.tooltip}
@@ -50,7 +84,7 @@ export default function DetailMaintanceCharge() {
                   place="right"
                   noArrow={true}
                   style={{
-                    width: "23rem",
+                    width: "40rem",
                     background: "#ffffff",
                     color: "#142337",
                     borderRadius: 0,
@@ -78,9 +112,39 @@ export default function DetailMaintanceCharge() {
               src={explanationIcon}
               alt="공용 관리비 설명"
               data-tooltip-id="totalCommonManagement"
-              data-tooltip-content={
-                "인건비 + 제사무비 + 제세공과금 + 피복비 + 교육훈련비 + 차량유지비 + 그 밖의 부대비용 + 청소비 + 경비비 + 소독비 + 승강기 유지비 + 지능형 네트워크 유지비 + 수선비 + 시설 유지비 + 안전 점검비 + 재해예방비 + 위탁관리 수수료"
-              }
+              data-tooltip-html={`<div style="display: flex; flex-direction: column; align-items: flex-start; gap: 2rem;">
+                    <span style="color: #142337; font-size: 1.6rem;">개별 사용료 상세</span>
+                    <ul style="display: flex; flex-direction: column; gap: 1.6rem; width: 100%;">
+                      <li style="width : 100%; display: flex; gap: 1rem;">
+                      <span style="color: #ffffff; padding: 0.6rem 1.2rem; background-color: #435464; border-radius: 1.4rem; height: fit-content; width: 15rem; text-align: center;">유지비</span>
+                        <ul style="display: flex; gap: 0.6rem; flex-wrap: wrap;">
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">청소비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">소독비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">수선비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">차량유지비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">시설유지비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">안전점검비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">재해예방비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">승강기유지비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">지능형네트워크유지비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">위탁관리 수수료</li>
+
+                        </ul>
+                      </li>
+                      <li style="width: 100%; display: flex; gap: 1rem;">
+                        <span style="position: relative; color: #ffffff; padding: 0.6rem 1.2rem; background-color: #435464; border-radius: 1.4rem; height: fit-content; width: 10rem; text-align: center;">일반</span>
+                        <ul style="display: flex; gap: 0.6rem; flex-wrap: wrap;">
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">인건비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">경비비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">제사무비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">제세공과금</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">피복비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">교육훈련비</li>
+                          <li style="border: 0.2rem solid #7F8A95; font-weight: 400; padding: 0.4rem 1.2rem; border-radius:1.4rem;">그 밖의 부대비용</li>
+                        </ul>
+                      </li>
+                    </ul>
+                    <div>`}
             />
             <Tooltip
               className={styles.tooltip}
@@ -88,7 +152,7 @@ export default function DetailMaintanceCharge() {
               place="right"
               noArrow={true}
               style={{
-                width: "23rem",
+                width: "40rem",
                 background: "#ffffff",
                 color: "#142337",
                 borderRadius: 0,
