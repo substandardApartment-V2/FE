41df507ae8be2ee904f0f21ed0332841b24e7482
exceed96 <exceed96@naver.com>
2025-03-19 22:36:26 +0900

    [fix] 네이버 지도 렌더링 및 뉴스 스타일 수정 #73

--- src/hooks/Map/useCreateMap.ts
+++ src/hooks/Map/useCreateMap.ts
@@ -1,39 +1,37 @@
 // 네이버 지도 생성 커스텀 훅
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { updateBounds } from "@/utils/map/updateBounds";
 import { useMarkerStore } from "@/store/useMarkerStore";
 import createCustumButton from "@/utils/map/createCustomButton";
 
 export default function useCreateMap() {
+  const [isLoading, setIsLoading] = useState(true);
   const mapRef = useRef<HTMLDivElement | null>(null);
   const setBounds = useMarkerStore((state) => state.setBounds);
   const setMap = useMarkerStore((state) => state.setMap);
-  const setIsLoading = useMarkerStore((state) => state.setIsLoading);
 
   // 지도 불러오기 성공
   const getSuccess = (position: GeolocationPosition) => {
-    if (useMarkerStore.getState().map) return;
+    setIsLoading(false);
+    if (useMarkerStore.getState().map || !mapRef.current) return;
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     const { naver } = window;
 
-    if (naver) setIsLoading(false);
-    if (naver && mapRef.current) {
-      const location = new naver.maps.LatLng(lat, lng);
-      const map = new naver.maps.Map(mapRef.current, {
-        center: location,
-        zoom: 15,
-      });
-      setMap(map);
-      createCustumButton(map);
-      updateBounds(map, setBounds);
-    }
+    const location = new naver.maps.LatLng(lat, lng);
+    const map = new naver.maps.Map(mapRef.current, {
+      center: location,
+      zoom: 15,
+    });
+    setMap(map);
+    createCustumButton(map);
+    updateBounds(map, setBounds);
   };
 
   const getError = () => {
     console.error("location error");
   };
 
-  return { mapRef, getSuccess, getError };
+  return { isLoading, mapRef, getSuccess, getError };
 }
